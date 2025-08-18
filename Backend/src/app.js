8640422d764f1e39726b91a2decb1ba0813a77d0
piyus1232 <166859560+piyus1232@users.noreply.github.com>
2025-08-18 19:08:50 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -30,7 +30,7 @@ app.use(
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: process.env.CLIENT_URL || "http://localhost:5173", 
     credentials: true, // allow cookies
   })
 );
